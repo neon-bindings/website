8f0f44a120b1cb0ca83970ee1429ddab0ccc9d0f
Amila Welihinda <amilajack@gmail.com>
2019-09-23 14:07:32 -0700

    add initial button hide / show examples

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -5,9 +5,11 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import withBaseUrl from '@docusaurus/withBaseUrl';
 import SyntaxHighlighter from 'react-syntax-highlighter';
+import { Button, ButtonGroup } from 'reactstrap';
 import styles from './styles.module.css';
 import '../css/custom.css';
 import siteConfig from '../../docusaurus.config';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const jsExample = `
 // JS
@@ -47,6 +49,42 @@ register_module!(mut cx, {
 })
 `;
 
+const asyncExample = `
+fn fibonacci_async(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+  let n = cx.argument::<JsNumber>(0)?.value() as usize;
+  let cb = cx.argument::<JsFunction>(1)?;
+
+  let task = FibonacciTask { argument: n };
+  task.schedule(cb);
+
+  Ok(cx.undefined())
+}
+`;
+
+const printArgsExample = `
+// Create a function that gets the number of arguments passed to it
+fn get_args_len(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    let args_length = cx.len();
+    println!("{}", args_length);
+    Ok(cx.number(args_length))
+}
+`;
+
+const codeExamples = [
+  {
+    name: 'Async Fibonacci',
+    code: asyncExample
+  },
+  {
+    name: 'Make Array',
+    code: neonExample
+  },
+  {
+    name: 'Print Function Arguments',
+    code: printArgsExample
+  }
+];
+
 const Logo = props => (
   <div className="neonProjectLogo">
     <div className="neon-logo">
@@ -67,57 +105,102 @@ const features = [
     )
   },
   {
-    title: <>'Guaranteed Safety'</>,
+    title: <>Guaranteed Safety</>,
     description: (
       <>
-        'If a neon module compiles, it is guaranteed to be memory safe by the
-        rust compiler'
+        If a neon module compiles, it is guaranteed to be memory safe by the
+        rust compiler
       </>
     ),
     imageUrl: 'img/checkmark.svg'
   },
   {
-    title: <>'Easy Parallelism'</>,
-    description: <>'Safely run multiple threads without data races'</>,
+    title: <>Easy Parallelism</>,
+    description: <>Safely run multiple threads without data races</>,
     imageUrl: 'img/fork.svg'
   }
 ];
 
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCode: codeExamples[0].code
+    };
+  }
+
+  onButtonClick(index) {
+    this.setState({
+      currentCode: codeExamples[index].code
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <ButtonGroup>
+          {codeExamples.map((example, idx) => (
+            <Button key={example.name} onClick={() => this.onButtonClick(idx)}>
+              {example.name}
+            </Button>
+          ))}
+        </ButtonGroup>
+        <SyntaxHighlighter language="rust">
+          {this.state.currentCode}
+        </SyntaxHighlighter>
+      </>
+    );
+  }
+}
+
 function Home() {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+      <header className={styles.header}>
         <div className="container">
           <Logo title={siteConfig.title} subtitle={siteConfig.tagline} />
+          <div className="row" style={{ textAlign: 'left' }}>
+            <SyntaxHighlighter
+              customStyle={{ fontSize: '0.8em' }}
+              language="javascript"
+              useInlineStyles
+            >
+              {jsExample}
+            </SyntaxHighlighter>
+            <SyntaxHighlighter
+              customStyle={{ fontSize: '0.8em' }}
+              language="rust"
+              useInlineStyles
+            >
+              {neonExample}
+            </SyntaxHighlighter>
+          </div>
           <div className={styles.buttons}>
-            <a href={withBaseUrl('docs/getting-started')}>
-              <button>Try It Out</button>
-            </a>
-            <a href={siteConfig.repoUrl}>
-              <button target="_blank">GitHub</button>
-            </a>
-            <a href="https://neon-bindings.com/api/neon/index.html">
-              <button target="_blank">API</button>
-            </a>
+            <ButtonGroup>
+              <a href={withBaseUrl('docs/getting-started')}>
+                <Button>Try It Out</Button>
+              </a>
+              <a href={siteConfig.repoUrl}>
+                <Button target="_blank">GitHub</Button>
+              </a>
+              <a href="https://neon-bindings.com/api/neon/index.html">
+                <Button target="_blank">API</Button>
+              </a>
+            </ButtonGroup>
           </div>
         </div>
       </header>
+
       <main>
         {features && features.length && (
           <section className={styles.features}>
             <div className="container">
-              <SyntaxHighlighter language="rust">
-                {exampleCode}
-              </SyntaxHighlighter>
-              <SyntaxHighlighter language="rust">{jsExample}</SyntaxHighlighter>
-              <SyntaxHighlighter language="rust">
-                {neonExample}
-              </SyntaxHighlighter>
               <div className="row">
                 {features.map(({ imageUrl, title, description }, idx) => (
                   <div
@@ -138,13 +221,27 @@ function Home() {
                   </div>
                 ))}
               </div>
-              <iframe src="https://asciinema.org/a/269799/iframe" />
             </div>
           </section>
         )}
+
+        <section className={styles.features}>
+          <div className="container">
+            <Carousel />
+          </div>
+        </section>
+
+        <section className={styles.features}>
+          <div className="container">
+            <iframe
+              src="https://asciinema.org/a/269799/iframe"
+              width="100%"
+              height="300px"
+            />
+          </div>
+        </section>
       </main>
     </Layout>
   );
 }
-
 export default Home;
