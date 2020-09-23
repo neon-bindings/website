81ba539ecb10209ecaa88670f6f248331470da03
David Herman <david.herman@gmail.com>
2020-09-23 09:42:54 -0700

    Oops, added missing syntax highlighting props modules.

---
+++ src/pages/neon-syntax.js
@@ -0,0 +1,98 @@
+export default {
+  "hljs-comment": {
+    color: "#7285b7",
+  },
+  "hljs-quote": {
+    color: "#7285b7",
+  },
+  "hljs-variable": {
+    color: "#ff9da4",
+  },
+  "hljs-template-variable": {
+    color: "#ff9da4",
+  },
+  "hljs-tag": {
+    color: "#ff9da4",
+  },
+  "hljs-name": {
+    color: "#ff9da4",
+  },
+  "hljs-selector-id": {
+    color: "#ff9da4",
+  },
+  "hljs-selector-class": {
+    color: "#ff9da4",
+  },
+  "hljs-regexp": {
+    color: "#ff9da4",
+  },
+  "hljs-deletion": {
+    color: "#ff9da4",
+  },
+  "hljs-number": {
+    color: "#4EBF22",
+    "font-weight": "bold",
+  },
+  "hljs-built_in": {
+    color: "#FFFF66",
+  },
+  "hljs-builtin-name": {
+    color: "#ffc58f",
+  },
+  "hljs-literal": {
+    color: "#8DE1FC",
+  },
+  "hljs-type": {
+    color: "#ffc58f",
+  },
+  "hljs-params": {
+    color: "#ffc58f",
+  },
+  "hljs-meta": {
+    color: "#ffc58f",
+  },
+  "hljs-link": {
+    color: "#ffc58f",
+  },
+  "hljs-attribute": {
+    color: "#ffeead",
+  },
+  "hljs-string": {
+    color: "#4EBF22",
+    "font-weight": "bold",
+  },
+  "hljs-symbol": {
+    color: "#d1f1a9",
+  },
+  "hljs-bullet": {
+    color: "#d1f1a9",
+  },
+  "hljs-addition": {
+    color: "#d1f1a9",
+  },
+  "hljs-title": {
+    color: "#8DE1FC",
+  },
+  "hljs-section": {
+    color: "#bbdaff",
+  },
+  "hljs-keyword": {
+    color: "#FF66FF",
+  },
+  "hljs-selector-tag": {
+    color: "#ebbbff",
+  },
+  hljs: {
+    display: "block",
+    overflowX: "auto",
+    background: "#002451",
+    color: "white",
+    padding: "0.5em",
+  },
+  "hljs-emphasis": {
+    fontStyle: "italic",
+  },
+  "hljs-strong": {
+    fontWeight: "bold",
+  },
+};
